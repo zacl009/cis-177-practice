d622d1a36462923c901aaa8f9377471519a030c6
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-03 13:44:41 -0600

    added notes

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// const name = "Zac";
+// let numOfYearsCoding = 1;
+
+// We are accessing an OBJECT called 'console' and using DOT NOTATION to access a KEY
+// That KEY is a METHOD, so we INVOKE it with ().
+// We also pass in an ARGUMENT as a STRING.
+// ARGUMENTS provide additional information that a METHOD needs to do its job.
+
+// console.log(
+//   "My name is " +
+//     name +
+//     " and I have been coding for " +
+//     numOfYearsCoding +
+//     " years."
+// );
+
+//Template Literals and Interpolation
+// console.log(
+//   `My name is ${name} and I have been coding for ${numOfYearsCoding} years.`
+// );
+
+// Create a variable 'timeOfDay' and assign it a string
+// const timeOfDay = "Morning";
+// console.log(`Good ${timeOfDay}`);
+
+// typeof tells what the variable is
+// console.log(typeof timeOfDay);
+// console.log(`The variable timeOfDay is of type: ${typeof timeOfDay}`);
+
+// JS will COERCE your data types into whatever it feels is appropriate.
+// Implicit Coercion
+// console.log("hello" + 22);
+// console.log(22 + 22);
+// console.log(22 + true);
+
+// Explicit Coercion
+// console.log(Number (false));
+
+// console.log(parseInt("123.45"));
+// console.log(parseFloat("123.45"));
+
+// console.log(Boolean(222));
