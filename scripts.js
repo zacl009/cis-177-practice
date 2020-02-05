cf52fdd253936b79e69ad228e1f8526ac1202e94
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-05 13:26:06 -0600

    Add examples of undefined, null and objects.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,21 @@
-// const name = "Zac";
-// let numOfYearsCoding = 1;
+// undefined is a PRIMITIVE TYPE.
+// It signifies the absence of ANY value;
+let x;
 
-// We are accessing an OBJECT called 'console' and using DOT NOTATION to access a KEY
-// That KEY is a METHOD, so we INVOKE it with ().
-// We also pass in an ARGUMENT as a STRING.
-// ARGUMENTS provide additional information that a METHOD needs to do its job.
+// NULL is a PRIMITIVE.
+// NULL represents a deliberate assignment of 'nothing.'
+console.log(x);
 
-// console.log(
-//   "My name is " +
-//     name +
-//     " and I have been coding for " +
-//     numOfYearsCoding +
-//     " years."
-// );
+// This is an OBJECT.
+const me = {
+  name: "Zac",
+  age: 22,
+  pets: {
+    cat: "Scratch"
+  }
+};
 
-//Template Literals and Interpolation
-// console.log(
-//   `My name is ${name} and I have been coding for ${numOfYearsCoding} years.`
-// );
-
-// Create a variable 'timeOfDay' and assign it a string
-// const timeOfDay = "Morning";
-// console.log(`Good ${timeOfDay}`);
-
-// typeof tells what the variable is
-// console.log(typeof timeOfDay);
-// console.log(`The variable timeOfDay is of type: ${typeof timeOfDay}`);
-
-// JS will COERCE your data types into whatever it feels is appropriate.
-// Implicit Coercion
-// console.log("hello" + 22);
-// console.log(22 + 22);
-// console.log(22 + true);
-
-// Explicit Coercion
-// console.log(Number (false));
-
-// console.log(parseInt("123.45"));
-// console.log(parseFloat("123.45"));
-
-// console.log(Boolean(222));
+// Create a new key value pair with age = 23.
+me.age = 23;
+console.log(me.pets.cat);
+console.log(me.pet.cat); // ! undefined.cat
