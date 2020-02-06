c618398d2f069ca430bd412cef109fd54062ceac
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-06 13:58:59 -0600

    Updated hw.js

---
+++ hw.js
@@ -0,0 +1,15 @@
+const bio = {
+  name: "Zac",
+  age: 22,
+  pets: {
+    cat: "Scratch",
+    dog: "Apollo"
+  }
+};
+
+console.log(bio.name);
+console.log(bio.age);
+console.log(bio.pets.dog);
+
+bio.age = 23;
+console.log(bio.age);
