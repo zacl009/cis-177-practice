da1520aed49fe95f2446b83fecd32e58b3618ab9
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-17 13:14:34 -0600

    Add examples of logical operators

---
+++ index.js
@@ -0,0 +1,12 @@
+const x = 3;
+const name = "hello";
+
+// TODO: Check if x is ODD AND if it's less than 10.
+if (x % 2 === 1 && x < 10) {
+  console.log("x is odd because it has a remainder of 1.");
+}
+
+// TODO: Check if x is ODD OR if it's less than 10.
+if (x % 2 === 1 || x < 10) {
+  console.log("x is odd because it has a remainder of 1.");
+}
