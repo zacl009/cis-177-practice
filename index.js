d70cf98feef614b0f3c9b198513b2899d1c579b8
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-19 13:31:10 -0600

    Added optimization for FizzBuzz solution using unary operator

--- index.js
+++ index.js
@@ -22,14 +22,26 @@ console.log(total);
 let i = 1;
 
 while (i <= 100) {
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (!(i % 3) && !(i % 5)) {
     console.log("FizzBuzz!");
-  } else if (i % 3 === 0) {
+  } else if (!(i % 3)) {
     console.log("Fizz!");
-  } else if (i % 5 === 0) {
+  } else if (!(i % 5)) {
     console.log("Buzz!");
   } else {
     console.log(i);
   }
   i += 1;
 }
+
+/**
+ * '!' is the NOT operator. It's a UNARY OPERATOR.
+ * Using this in front of any data type (such as a number)
+ * will immediately convert that date type into a boolean.
+ * Then, that value will be inversed as per the NOT operator.
+ */
+console.log(!0);
+console.log(!111);
+console.log(!"Hello");
+console.log(!"");
+console.log(!true);
