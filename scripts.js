b4b4a32766037265974f0f8858005d415f57db29
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-24 13:41:37 -0600

    Show examples of for loops

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-const person = {};
+// Log the numbers from 1 to 10.
+// In parenthesis, specify 3 things - start - stop - increment
+// 'for' limits the use of GLOBAL VARIABLE for i
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
-// Prompt is a BLOCKING operation.
-person.name = prompt("What is your name?");
-person.age = prompt("What is your age?");
-person.favWrestler = prompt("What is your favorite wrestler?");
-alert(`Hello ${name}`);
+// Add numbers from 1 to 10 using 'for'
+let total = 0;
+for (let i = 1; i <= 10; i++) {
+  console.log(total);
+  total += i;
+}
+
+// Fizz Buzz
+// Log the numbers from 1 to 100.
+// However, if the number is divisible by 3, log 'Fizz!'
+// If the number is divisible by 5, log 'Buzz!'
+// If the number is divisible by 3 and 5, log 'FizzBuzz!'
+// Each number should only result in 1 log.
