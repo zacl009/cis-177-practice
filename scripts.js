ff243bd453e02e4baed614c712f82986f08e968a
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-26 13:46:01 -0600

    Add another blank prompting example with numbers

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
-// IMPLICIT CONVERSION
-// || means OR
-// If first EXPRESSION is 'truthy', then we don't EVALUATE the right hand side.
-// 'name' wil lbe a STRING, but it might be blank.
+let age = prompt("What is your age?");
 
-//If 'prompt' RETURNS an empty STRING - b/c of the '||' this will be IMPLICITLY CONVERTED to 'false'
-let name = prompt("What is your name?");
+age = Number(age);
 
-// If 'name' is blank, then '!' will convert to a Boolean (NOT FALSE)
-// If 'name' is not blank, then '!' will convert to a Boolean (NOT TRUE)
-while (!name) {
-  // while (name === "")
-  name = prompt("What is your name?");
+console.log(Boolean(NaN));
+
+console.log(Boolean(0));
+
+// TODO: If the user enters something that EXPLICITLY CONVERTS to 'NaN', alert them.
+if (!age) {
+  alert("This is not a number!");
 }
-
-console.log(name);
