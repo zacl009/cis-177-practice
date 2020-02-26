9ae1e70bcf7876bf5360e6e6e892b660d83e6f89
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-25 22:55:58 -0600

    Added FizzBuzz for loop and confirm() boolean value

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-/**
- * TODO: Sum the numbers from 1 to 10.
- */
-
-let x = 0;
-let total = 0;
-
-while (x <= 10) {
-  total += x; // total = total + x;
-  x += 1;
-}
-
-console.log(total);
-
 // Fizz Buzz
 // Log the numbers from 1 to 100.
 // However, if the number is divisible by 3, log 'Fizz!'
 // If the number is divisible by 5, log 'Buzz!'
 // If the number is divisible by 3 and 5, log 'FizzBuzz!'
 // Each number should only result in 1 log.
-
-let i = 1;
-
-while (i <= 100) {
+for (let i = 1; i <= 100; i += 1) {
   if (!(i % 3) && !(i % 5)) {
     console.log("FizzBuzz!");
   } else if (!(i % 3)) {
@@ -31,17 +14,4 @@ while (i <= 100) {
   } else {
     console.log(i);
   }
-  i += 1;
 }
-
-/**
- * '!' is the NOT operator. It's a UNARY OPERATOR.
- * Using this in front of any data type (such as a number)
- * will immediately convert that date type into a boolean.
- * Then, that value will be inversed as per the NOT operator.
- */
-console.log(!0);
-console.log(!111);
-console.log(!"Hello");
-console.log(!"");
-console.log(!true);
--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-// Log the numbers from 1 to 10.
-// In parenthesis, specify 3 things - start - stop - increment
-// 'for' limits the use of GLOBAL VARIABLE for i
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-
-// Add numbers from 1 to 10 using 'for'
-let total = 0;
-for (let i = 1; i <= 10; i++) {
-  console.log(total);
-  total += i;
-}
-
-// Fizz Buzz
-// Log the numbers from 1 to 100.
-// However, if the number is divisible by 3, log 'Fizz!'
-// If the number is divisible by 5, log 'Buzz!'
-// If the number is divisible by 3 and 5, log 'FizzBuzz!'
-// Each number should only result in 1 log.
+leave = {
+  isConfirmed: window.confirm("Do you really want to leave?")
+};
