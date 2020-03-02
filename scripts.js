ac22ddec9d36a0eab195b4e2ccaf81adab6f536f
Zac Lopez <zachary.lopez@student.swic.edu>
2020-03-02 13:46:13 -0600

    Add notes on arrays

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
-let age = prompt("What is your age?");
+const myArray = ["String" 23, "Another String", true, false];
 
-age = Number(age);
+/**
+ * the LENGTH of an ARRAY is always 1 less than the highest index.
+ */
 
-console.log(Boolean(NaN));
+console.log(myArray[1]); // 23
+console.log(myArray.length); // 5
 
-console.log(Boolean(0));
+console.log(myArray[99]); // undefined
+myArray[1] = 177;
+myArray[1000] = "James";
+console.log(myArray[1000]); // James
+console.log(myArray.length); // 1001
+console.log(myArray[888]);
 
-// TODO: If the user enters something that EXPLICITLY CONVERTS to 'NaN', alert them.
-if (!age) {
-  alert("This is not a number!");
+
+const data = [
+  {name: "Zac", hobbies: ["eating", "cooking", "poker"]},
+  {name: "Alex"},
+  {name: "James"}
+]
+
+// log poker
+console.log(data[0].hobbies[2]);
+
+// Iterate over data and log every name.
+for (let i = 0; i <= data.length - 1 ;i++) {
+  console.log(data[i].name);
 }
